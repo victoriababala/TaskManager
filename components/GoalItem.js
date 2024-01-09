3fcd90d70520962c7462ee7712dcf51ba92f0467
victoriababala <victoriababala1106@gmail.com>
2024-01-09 17:25:06 +0200

    first commit

---
+++ components/GoalItem.js
@@ -0,0 +1,32 @@
+import { StyleSheet, View, Text, Pressable } from "react-native";
+
+function GoalItem(props) {
+  return (
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: "#221448" }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        style={({ pressed }) => pressed && props.pressedItem}
+      >
+        <Text style={styles.goalText}>{props.text}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+export default GoalItem;
+const styles = StyleSheet.create({
+  goalItem: {
+    margin: 8,
+
+    borderRadius: 6,
+    backgroundColor: "#5539a3",
+  },
+  pressedItem: {
+    opacity: 0.5,
+  },
+  goalText: {
+    color: "white",
+    padding: 8,
+  },
+});
